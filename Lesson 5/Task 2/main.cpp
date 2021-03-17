4fff654996f51c6e72354ae76bc5f1da63f14091
Harvey <harveymurray@outlook.com>
2021-03-17 11:50:50 +0000

    Added comments

--- Lesson 5/Task 2/main.cpp
+++ Lesson 5/Task 2/main.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
+  // Decalring variables
   int year, month, days;
 
+  // Getting input from user
   cout << "Enter a year: ";
   cin >> year;
   cout << "Enter a month [1-12]: ";
   cin >> month;
 
+  // Checking what month it is and assigning how many days are in that month
   if (month == 2)
   {
     if (year % 4 == 0)
@@ -31,6 +34,7 @@ int main()
     days = 31;
   }
 
+  // Displaying the month and how many days it has
   cout << "Month " << month << " has " << days << " days";
 
   return 0;
