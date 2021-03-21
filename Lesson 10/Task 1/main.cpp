67345037c2f11d07261247eaab8a9bc98f0a786e
Harvey <harveymurray@outlook.com>
2021-03-21 16:58:20 +0000

    Lesson 10, Task 1

--- Lesson 10/Task 1/main.cpp
+++ Lesson 10/Task 1/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
+  string word;
+  int number;
+
+  cout << "Enter word: ";
+  cin >> word;
+
+  cout << "Enter number: ";
+  cin >> number;
+
+  string newWord = word.substr(0, number);
+  cout << newWord;
 
   return 0;
 }
